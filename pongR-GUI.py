8e2a556052ffd73535afe14f2bf2fca764dc74f8
Austin Scott <13ascott@gmail.com>
2021-11-10 17:48:36 -0500

    first commit with basic mariadb connection

---
+++ pongR-GUI.py
@@ -0,0 +1,19 @@
+""" Script to run the python/Kivy GUI to interact with the local MariaDB database titled "compoundpingpong" """
+
+import mariadb
+import sys
+
+try:
+    conn = mariadb.connect(user="localuser",password="Welcome1",host="localhost",port=3306,database="compoundpingpong")
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB: {e}")
+    sys.exit(1)
+
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM players")
+
+for idnum, lastname, firstname, birthdate, profpic in cur:
+    print(f"{firstname} {lastname}")
+
+conn.close()
